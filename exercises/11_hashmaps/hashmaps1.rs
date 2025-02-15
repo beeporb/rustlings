852ba117fddad0de2b8d43ccc069ab01a9202986
Beeporb <backplateorbit@gmail.com>
2025-02-15 13:27:46 +0000

    feat: some hashmaps solution

--- exercises/11_hashmaps/hashmaps1.rs
+++ exercises/11_hashmaps/hashmaps1.rs
@@ -10,10 +10,17 @@ fn fruit_basket() -> HashMap<String, u32> {
     // TODO: Declare the hash map.
     // let mut basket =
 
+    let mut basket = HashMap::<String, u32>::new();
+
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket.
+
+    basket.insert(String::from("apple"), 3);
+    basket.insert(String::from("dragonfruit"), 4);
+    basket.insert(String::from("orange"), 5);
+    basket.insert(String::from("kiwi"), 6);
 
     basket
 }
