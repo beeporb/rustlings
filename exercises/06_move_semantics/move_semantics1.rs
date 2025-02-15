57e42edb7e2e853d6718156d373c274521e476ca
Beeporb <backplateorbit@gmail.com>
2025-02-15 11:37:49 +0000

    feat: even further still solutions with notes on borrowing

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -1,6 +1,14 @@
 // TODO: Fix the compiler error in this function.
+// Solution
+// Make the shadowed vec mutable.
+// If you then try to use vec0, you'll get an error because vec0 has been "moved".
+// This is because you have provided the value to the function by Value.
+// Because Vec does not implement Copy, it is instead Moved to the function, where
+// ownership is taken.
+// If Vec did implement Copy, then the compiler would see this and the value would be
+// copied. You'd then still be able to use the old version vec[0].
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+    let mut vec = vec;
 
     vec.push(88);
 
@@ -9,6 +17,9 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
 fn main() {
     // You can optionally experiment here.
+
+    let vec0: Vec<i32> = vec![22,44,66];
+    
 }
 
 #[cfg(test)]
--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -8,6 +8,17 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
 fn main() {
     // You can optionally experiment here.
+    let vec0 = vec![22,44,66];
+    let vec0_copy = vec0.clone();
+    let vec1 = fill_vec(vec0_copy);
+
+    // Doing something like this also doesn't work.
+    // When we assign vec0_copy we're actually borrowing vec0.
+    // This therefore means that vec0[0] would be attempting to access
+    // the value after it has been borrowed.
+    // We would need to use .clone() in this case too.
+    println!("{}", vec0[0]);
+    println!("{}", vec1[0]);
 }
 
 #[cfg(test)]
@@ -16,11 +27,16 @@ mod tests {
 
     // TODO: Make both vectors `vec0` and `vec1` accessible at the same time to
     // fix the compiler error in the test.
+    // Solution
+    // You can use the .clone() function on a vector to return a copy of it.
+    // This means that both vec0 and vec1 are now pointing to different data.
+    // Passing vec0.clone() into the function isn't passing the value to be copied,
+    // but a clone of the value.
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
